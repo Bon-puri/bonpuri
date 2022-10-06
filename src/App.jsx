adbb375ebbbaf1a4869b5343ed67d16ea931860e
JoJosSidekick <114755556+JoJoSidekick@users.noreply.github.com>
2022-10-05 21:06:36 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,213 @@
+
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen flex flex-col text-black background-gradient">
+			<div className="header-img  text-center" >
+        <h1 class = "mt-20">Bon-puri</h1>
+		
+			</div>
+			
+			<main className="container mx-auto px-6 pt-16 flex-1 text-center text-black">
+				<div className="flex flex-col md:flex-row items-center justify-between pb-12">
+						<p className="mb-4 md:mb-0 md:text-xl">Built with 💖</p>
+						<div className="flex -mx-6 text-black space-x-2 ">
+							<button class="button button-blue " data-ripple-light="true">
+								<a href="https://twitter.com/BonPuri_coin" className="mx-3 hover:opacity-80 duration-150">Twitter</a>
+							</button>
+              <button class="button button-pink" data-ripple-light="true">
+                <a href="https://medium.com/@bonpuri">Medium</a>
+              </button>
+								<button class="button button-gradient button-pink" data-ripple-light="true">
+								<a href="https://etherscan.io/address/0xbdd9176436a0ff67eeb270d9518d4b449d7018b1" className="mx-3 hover:opacity-80 duration-150">Etherscan</a>
+							</button>
+							
+							<button class="button button-gradient button-pink" data-ripple-light="true">
+								<a href="https://app.uniswap.org/#/swap" className="mx-3 hover:opacity-80 duration-150">Uniswap</a>
+							</button>	
+						</div>
+				</div>
+			
+				<div class="card card-plain mb-4">
+				
+				<div class="flex">
+					<div class="w-2/3 bp5-img">
+					<img
+						class="jump" 
+					/>
+					</div>
+					<div class="card-body w-2/3 ">
+					<h1 class="text-black">$BONPURI</h1>
+					<a href="#">
+						<h3 class="mt-4 opacity-100 text-blue-500">The Origin of Worlds</h3>
+					</a>
+					<p class="text-xl text-blue-500 mt-6 opacity-95 font-italic">
+						First, a seed must be planted
+					</p>
+
+					<p class="mt-4 mb-5 opacity-80 text-black">
+						<p>Bon-puri is the Korean myth of the world's creation</p>  
+						
+						<p>------------------------------------------------------------------</p>
+						<p>Contract Address:</p>
+						<p class = "text-xl mb-10">
+						0xBDD9176436a0Ff67EEB270D9518d4B449d7018b1
+						</p>
+            			<p>We begin with 1.</p>  
+						<p>1 and 1 make 2.</p>
+            			<p>2 and 1 make 3.</p>
+            			<p>From 3, all things are made.</p>
+            
+
+
+					<p class="ready"></p>	
+
+					</p>
+					
+					</div>
+				</div>
+				</div>
+
+
+				
+				<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-40 w-fit mx-auto mb-8 rounded-full">
+				To speak of the cosmic totality: when the four corners were shrouded in sleep because heaven and earth had no gawp,
+				I saw that the universe was a single bundle then.
+				</div>
+				<div>
+					<p></p>
+					<p></p>
+				</div>
+
+				<div className="columns-2 gap-8 space-y-32 pt-16">
+					<div class="card">
+						<div class="card-header mx-4 -mt-6 ">
+							<a href="#" blur-shadow-image="true">
+							<img class="w-auto rounded-lg bp1-img" />
+							</a>
+						</div>
+						<div class="card-body">
+							<a href="#">
+							<h4 class="font-medium">4/4 Taxes</h4>
+							</a>
+							
+							<p class="opcacity-60 mb-3">
+								<p>Breakdown:</p>
+								<p>1% Automatically sent to LP</p>
+								<p>3% Developing & Marketing</p> 
+							</p>
+						<button class="button button-pink" data-ripple-light="true">
+						<a href="https://etherscan.io/address/0xbdd9176436a0ff67eeb270d9518d4b449d7018b1">View Contract</a>
+						</button>
+						</div>
+					</div>
+					<div class="card">
+						<div class="card-header mx-4 -mt-6 ">
+							<a href="#" blur-shadow-image="true">
+							<img class="w-auto rounded-lg bp6-img" />
+							</a>
+						</div>
+						<div class="card-body">
+							<a href="#">
+							<h4 class="font-medium">Tokenomics</h4>
+							</a>
+							<p class="opcacity-60 mb-3">
+							<p>Total Supply: 999,999,999</p>
+							<p>Supplied & Locked to LP: 999,999,999</p>
+							<p>No Dev Wallet</p>
+							<p>No Mint, No Burn</p>
+							</p>
+							
+						<button class="button button-pink" data-ripple-light="true">
+						<a href="https://app.unicrypt.network/amm/uni-v2/pair/0x68cBFdc9F889C1476BB3FB2FA974F733bC041d7D">View LP Lock</a>
+						</button>
+						</div>
+					</div>
+
+					<div class="card">
+						<div class="card-header mx-4 -mt-6 ">
+							<a href="#" blur-shadow-image="true">
+							<img class="w-auto rounded-lg bp2-img" />
+							</a>
+						</div>
+						<div class="card-body">
+							<a href="#">
+							<h4 class="font-medium">Ethos</h4>
+							</a>
+							<p class="opcacity-60 mb-3">
+							
+							<p>To love all living things,</p>
+							<p>and treat them well, without thought of the self,</p> 
+							<p>this is good and just.</p>
+							</p>
+						<button class="button button-pink" data-ripple-light="true">
+						<a href="https://www.unl.edu/prodmgr/NRT/Tao%20Te%20Ching%20-%20trans.%20by%20J.H..%20McDonald.pdf">Read the Tao te Ching</a>
+						</button>
+						</div>
+					</div>
+					<div class="card">
+						<div class="card-header mx-4 -mt-6">
+							<a href="#" blur-shadow-image="true">
+							<img class="w-auto rounded-lg bp4-img" />
+							</a>
+						</div>
+						<div class="card-body">
+							<a href="#">
+							<h4 class="font-medium">Development</h4>
+							</a>
+							<p class="opcacity-60 mb-3">
+							<p>Life is an opportunity for your soul to learn new things</p>
+							<p>First, the community must grow naturally</p>
+							<p>When the time is right, bloom and let them eat your fruit</p>
+							<p>Developments will publish on Medium</p>
+							</p>
+							
+						<button class="button button-pink" data-ripple-light="true">
+						<a href="https://medium.com/@bonpuri">Visit Medium</a>
+						</button>
+						</div>
+					</div>
+
+				</div>	
+
+				<div class="card card-plain mb-8">
+				
+				<div class="flex">
+					<div class="w-2/3">
+					<img
+						class="rounded-lg -mt-28 " 
+					/>
+					</div>
+					<div class="card-body w-2/3 -mb-24  ">
+					
+					
+					</div>
+				</div>
+				</div>
+
+				
+				
+				
+					
+					
+					
+				
+
+
+				
+				
+
+
+
+			</main>
+			
+			<footer className="container mx-auto p-6">
+				
+			</footer>
+		</div>
+  )
+}
+
+export default App
